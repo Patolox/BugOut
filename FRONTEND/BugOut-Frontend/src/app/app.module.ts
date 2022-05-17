314e60e4ed2bb5b06b57ca47c65a735b3d07a63c
Bruno Andrade <bhmandrade411@gmail.com>
2022-05-17 12:08:41 -0300

    Configuração do arquivo de rotas.

--- FRONTEND/BugOut-Frontend/src/app/app.module.ts
+++ FRONTEND/BugOut-Frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatSidenavModule
+    MatSidenavModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
