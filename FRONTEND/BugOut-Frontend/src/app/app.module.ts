769b5741d779d4c9404b6747214050bf5c33b801
Cicero <titofera4@gmail.com>
2022-05-20 20:08:59 -0300

    Adiciona HTML das colunas

--- FRONTEND/BugOut-Frontend/src/app/app.module.ts
+++ FRONTEND/BugOut-Frontend/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatMenuModule } from '@angular/material/menu';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
